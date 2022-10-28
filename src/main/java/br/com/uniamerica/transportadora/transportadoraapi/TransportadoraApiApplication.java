a47ca1f38f9a374b182775349ba20b2a8e9c5f7e
Allyson <jandreyallyson@gmail.com>
2022-10-28 12:31:43 -0300

    Formatação

--- src/main/java/br/com/uniamerica/transportadora/transportadoraapi/TransportadoraApiApplication.java
+++ src/main/java/br/com/uniamerica/transportadora/transportadoraapi/TransportadoraApiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransportadoraApiApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-		SpringApplication.run(TransportadoraApiApplication.class, args);
-	}
+        SpringApplication.run(TransportadoraApiApplication.class, args);
+    }
 
 }
